e98788e96e0d818473188ebc1554cfd94f82f878
tr920 <isosoman@gmail.com>
2019-11-17 21:04:27 +0800

    no message

---
+++ untitled_check_ubiqfile.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Get HTML of page from user's input, get all of the image links, and make sure URLs have HTTPs
+#curl $1 | grep -E "(https?:)?//[^/\s]+/\S+\.(jpg|png|gif)" -o | sed "s/^(https?)?\/\//https\:\/\//g" -r > urls.txt
+#get the line contain parameter about hash and ID info
+#sleep $[ ( $RANDOM % 10 )  ]s
+
+
+echo $1
+c="$(curl $1 | grep -e 'ubiqfile.com' | sed "s/.*http/http/g"|sed "s/\" rel.*//g")"
+echo c is  ${c}
+if  [ -z "$c" ]; then
+   echo without URL
+   echo "==" >> temp
+   exit 1
+fi
+
+echo ${c}  >> temp
