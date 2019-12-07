36f314e8638057fba64001b6f3333b17754c406e
tr920 <isosoman@gmail.com>
2019-12-07 22:06:45 +0800

    no message

--- untitled_ubiq.sh
+++ untitled_ubiq.sh
@@ -19,40 +19,11 @@ do
       echo file is exist
       continue
    fi
-   d=`date +%s`
-   echo d is ${d}
-   curl ${c} | grep -e 'download_video' | sed "s/.*download_video(//g" | sed "s/)\">.*//g" | sed "s/'//g"  > ${d}
-    
-   n=0
-   a=./urls.txt
-   b=0
-   echo url_include_hash is `cat ${d}`
-   e=0
-   #get the ID info to $b and get the hash to $c
-   for i in `cat ${d} | tr ',' '\n'` ; do
-      let n=$n+1
-      var=`echo "var${n}"`
-      echo $var is ... ${i}
-      if [ $n -eq 1 ]
-      then
-         #statementsb
-         b=${i}
-         #echo b is ...${b}
-      elif [ $n -eq 3 ]; then
-         c=${i}
-      fi
-   done
-#merge to complete URL
-   url="https://gounlimited.to/dl?op=download_orig&id="${b}"&mode=n&hash="${c}""
-#show URL
-   echo url is ...${url}
-# require the realy video file URL
-   curl ${url}  --cookie cookies.txt | grep -e 'Direct Download Link' | sed "s/.*href=\"//g" |  sed "s/\">D.*//g" > ${d} 
-   cat  ${d} | sed 's/.*\///g'
+  
 #get the video file
    if ! [ -f ./`cat ${d}|sed 's/.*\///g'` ]; then
       touch `cat ${d}|sed 's/.*\///g'`
-      wget -i ${d} -c
+      curl --cookie cookies_ubiq.txt  -J -L ${c} -C - --output ${d} 
    fi
    rm ${d}
 
